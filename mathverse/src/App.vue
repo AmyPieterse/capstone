af7407c222276160a288ae82030fa8896d03e936
AmyPieterse <amypieterse.15@gmail.com>
2023-08-30 16:34:19 +0200

    frontend file structure

---
+++ mathverse/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+
+    </footer>
+  </div>
+</template>
+
+<style>
+
+</style>
